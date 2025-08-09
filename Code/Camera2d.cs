6c591fb596ff2df40d401acfca0b350bc4cd575f
christlm <christlm@fim.uni-passau.de>
2025-08-09 12:01:19 +0200

    camera movement

--- Code/Camera2d.cs
+++ Code/Camera2d.cs
@@ -1,10 +1,17 @@
 using Godot;
 using System;
+using NewGameProject.sim;
 
 public partial class Camera2d : Camera2D
 {
     [Export] private Vector2 _zoomSpeed = Vector2.One;
+    private const int scroll_pixels = 1000;
     
+    private Vector2 leftstep = new Vector2(-scroll_pixels, 0);
+    private Vector2 rightstep = new Vector2(scroll_pixels, 0);
+    private Vector2 upstep = new Vector2(0, -scroll_pixels);
+    private Vector2 downstep = new Vector2(0, scroll_pixels);
+
     public override void _Process(double delta)
     {
         base._Process(delta);
@@ -14,6 +21,18 @@ public partial class Camera2d : Camera2D
         } else if (Input.IsActionJustPressed("zoom out"))
         {
             Zoom += _zoomSpeed * (float) delta;
+        } else if (Input.IsActionPressed("camera up"))
+        {
+            Position += upstep * (float) delta;
+        } else if (Input.IsActionPressed("camera down"))
+        {
+            Position += downstep * (float) delta;
+        } else if (Input.IsActionPressed("camera left"))
+        {
+            Position += leftstep * (float) delta;
+        } else if (Input.IsActionPressed("camera right"))
+        {
+            Position += rightstep * (float) delta;
         }
     }
 }
