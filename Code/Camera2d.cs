710abe83dee62ad1cf4e28c9ec9a72a4db1ef0e6
Nico Philippi <78383233+nicophilippi@users.noreply.github.com>
2025-08-09 12:09:38 +0200

    Camera Zoom fix

--- Code/Camera2d.cs
+++ Code/Camera2d.cs
@@ -1,10 +1,10 @@
 using Godot;
-using System;
-using NewGameProject.sim;
 
 public partial class Camera2d : Camera2D
 {
     [Export] private Vector2 _zoomSpeed = Vector2.One;
+    [Export] private float _zoomMin = 0.5f;
+    [Export] private float _zoomMax = 2f;
     private const int scroll_pixels = 1000;
     
     private Vector2 leftstep = new Vector2(-scroll_pixels, 0);
@@ -17,10 +17,10 @@ public partial class Camera2d : Camera2D
         base._Process(delta);
         if (Input.IsActionJustPressed("zoom in"))
         {
-            Zoom -= _zoomSpeed * (float) delta;
+            Zoom -= _zoomSpeed;
         } else if (Input.IsActionJustPressed("zoom out"))
         {
-            Zoom += _zoomSpeed * (float) delta;
+            Zoom += _zoomSpeed;
         } else if (Input.IsActionPressed("camera up"))
         {
             Position += upstep * (float) delta;
@@ -34,5 +34,8 @@ public partial class Camera2d : Camera2D
         {
             Position += rightstep * (float) delta;
         }
+
+        Zoom = new Vector2(Mathf.Clamp(Zoom.X, _zoomMin, _zoomMax),
+            Mathf.Clamp(Zoom.Y, _zoomMin, _zoomMax));
     }
 }
