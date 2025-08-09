1110fb35d95f88f93c5afdbc8aadfb636103e9ed
Nico Philippi <78383233+nicophilippi@users.noreply.github.com>
2025-08-09 11:32:30 +0200

    CardManager: Now zoom with Camera

---
+++ Code/Tooltip.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+public partial class Tooltip : Control
+{
+    [Export] private RichTextLabel _txtNode;
+    
+    
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        
+        
+        
+    }
+}
